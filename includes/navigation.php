f1f1770f0e3c3c2c1a8895f55e5c0141ef4e5d31
Antonio Gamba-Bari <lodeanto@gmail.com>
2013-11-01 16:37:27 -0400

    changed relative urls to the nav bar

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,16 +1,16 @@
 <nav><!-- top nav -->
 	<div class="menu">
 		<ul>
-			<li><a href="../index.php">Home</a></li>
-			<li><a href="../tour.php">Tour</a></li>
-			<li><a href="../explore.php">Explore</a></li>
-			<!-- <li><a href="../research.php">Resources</a></li> -->
-			<li><a href="../contribute.php">Contribute</a></li>
-			<li><a href="../technical.php">Technical</a></li>
-			<li><a href="../faq.php">FAQ</a></li>
-			<li><a href="../news-and-events.php">News & Events</a></li>
-			<li><a href="../about.php">About</a></li>
-			<li><a href="../contact.php">Contact</a></li>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="tour.php">Tour</a></li>
+			<li><a href="explore.php">Explore</a></li>
+			<!-- <li><a href="research.php">Resources</a></li> -->
+			<li><a href="contribute.php">Contribute</a></li>
+			<li><a href="technical.php">Technical</a></li>
+			<li><a href="faq.php">FAQ</a></li>
+			<li><a href="news-and-events.php">News & Events</a></li>
+			<li><a href="about.php">About</a></li>
+			<li><a href="contact.php">Contact</a></li>
 		</ul>
 	</div>
 </nav><!-- end of top nav -->
