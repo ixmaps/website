dd5969fee9d00177feeae44713444a996ec4bb6b
Antonio Gamba-Bari <lodeanto@gmail.com>
2013-11-04 18:31:48 -0500

    assuring that tr detail win opens flag win

--- js/tr-detail.js
+++ js/tr-detail.js
@@ -16,12 +16,19 @@ var totR = jQuery('table:eq(1) tr').length;
 //console.log('totR',totR);
   // add flagging column
   jQuery("table:eq(1) tr:first").append("<th width='115px'><a href=''>Flag</a></th>");
+  
+  // get trId from table
+  var trId = jQuery('table:eq(0) tr:eq(0) td:eq(1)').html();
+
   // get ip value for each row and add flag link on last column
   for (var i=0;i<totR;i++)
   { 
     if(i>0){
       var ip = jQuery('table:eq(1) tr:eq('+i+') td:eq(1)').html();
-      //console.log(ip);
+      var hopN = jQuery('table:eq(1) tr:eq('+i+') td:eq(0)').html();
+      
+      
+      console.log(ip, trId, hopN);
       //jQuery("table:eq(1) tr:gt(0)").append("<td>Flag This</td>");
       jQuery("table:eq(1) tr:eq("+i+")").append('<td><a class="text-new" href="javascript:showFlagsParent(\''+ip+'\')">Flag This IP</a></td>');
     }      
