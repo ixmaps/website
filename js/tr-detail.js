8a11a3aff37f0f9fa9cee9cea1c5b2d30e15641c
Antonio Gamba-Bari <lodeanto@gmail.com>
2013-11-04 19:03:34 -0500

    fixed flagging on tr-detail

--- js/tr-detail.js
+++ js/tr-detail.js
@@ -3,17 +3,9 @@ jQuery(document).ready(function() {
 });
 
 var addFlagIpLinks = function(){
-  //alert('page loaded');
 
-
-//var t = jQuery('table tr:first td');
-//console.log(t[1]);
-
-//var c = jQuery("#tr-details-tab tr:first td").length;
-//var c = jQuery('table:eq(1) tr:first td').length;
 // get number of routers
 var totR = jQuery('table:eq(1) tr').length;
-//console.log('totR',totR);
   // add flagging column
   jQuery("table:eq(1) tr:first").append("<th width='115px'><a href=''>Flag</a></th>");
   
@@ -29,17 +21,11 @@ var totR = jQuery('table:eq(1) tr').length;
     if(i>0){
       var ip = jQuery('table:eq(1) tr:eq('+i+') td:eq(1)').html();
       var hopN = jQuery('table:eq(1) tr:eq('+i+') td:eq(0)').html();
-      
-      
-      console.log(ip, trId, hopN);
-      //jQuery("table:eq(1) tr:gt(0)").append("<td>Flag This</td>");
       jQuery("table:eq(1) tr:eq("+i+")").append('<td><a class="text-new" href="javascript:showFlagsParent('+trId+','+hopN+','+'\''+ip+'\''+',true'+')">Flag This IP</a></td>');
     }      
-    
   }
   
 }
-var showFlagsParent = function (ip){
-  parent.showFlags(ip);
-  console.log('IP to flag', ip);
+var showFlagsParent = function (trId, hopN, ip, openFlagWin){
+  parent.showFlags(trId, hopN, ip, openFlagWin);
 }
