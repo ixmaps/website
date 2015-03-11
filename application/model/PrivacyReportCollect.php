b4ece60437185e7735fa496b195e3b9e97ad7486
Antonio Gamba-Bari <lodeanto@gmail.com>
2015-03-11 04:25:59 -0400

    added getPrivacyDataImport

--- application/model/PrivacyReportCollect.php
+++ application/model/PrivacyReportCollect.php
@@ -17,16 +17,16 @@ class PrivacyReportCollect
 		$csvData = "";
 		foreach ($data as $key => $value) {
 			$c++;
-			$csvData .="\n(".$c.",1,".$value["asn"].",'".$value["carrier_name"]."',".$value["score1"].",".$value["created_by"]."'),";
-			$csvData .="\n(".$c.",2,".$value["asn"].",'".$value["carrier_name"]."',".$value["score2"].",".$value["created_by"]."'),";
-			$csvData .="\n(".$c.",3,".$value["asn"].",'".$value["carrier_name"]."',".$value["score3"].",".$value["created_by"]."'),";
-			$csvData .="\n(".$c.",4,".$value["asn"].",'".$value["carrier_name"]."',".$value["score4"].",".$value["created_by"]."'),";
-			$csvData .="\n(".$c.",5,".$value["asn"].",'".$value["carrier_name"]."',".$value["score5"].",".$value["created_by"]."'),";
-			$csvData .="\n(".$c.",6,".$value["asn"].",'".$value["carrier_name"]."',".$value["score6"].",".$value["created_by"]."'),";
-			$csvData .="\n(".$c.",7,".$value["asn"].",'".$value["carrier_name"]."',".$value["score7"].",".$value["created_by"]."'),";
-			$csvData .="\n(".$c.",8,".$value["asn"].",'".$value["carrier_name"]."',".$value["score8"].",".$value["created_by"]."'),";
-			$csvData .="\n(".$c.",9,".$value["asn"].",'".$value["carrier_name"]."',".$value["score9"].",".$value["created_by"]."'),";
-			$csvData .="\n(".$c.",10,".$value["asn"].",'".$value["carrier_name"]."',".$value["score10"].",".$value["created_by"]."'),";			
+			$csvData .="\n(".$c.",1,".$value["asn"].",'".$value["carrier_name"]."',".$value["score1"].",'".$value["created_by"]."'),";
+			$csvData .="\n(".$c.",2,".$value["asn"].",'".$value["carrier_name"]."',".$value["score2"].",'".$value["created_by"]."'),";
+			$csvData .="\n(".$c.",3,".$value["asn"].",'".$value["carrier_name"]."',".$value["score3"].",'".$value["created_by"]."'),";
+			$csvData .="\n(".$c.",4,".$value["asn"].",'".$value["carrier_name"]."',".$value["score4"].",'".$value["created_by"]."'),";
+			$csvData .="\n(".$c.",5,".$value["asn"].",'".$value["carrier_name"]."',".$value["score5"].",'".$value["created_by"]."'),";
+			$csvData .="\n(".$c.",6,".$value["asn"].",'".$value["carrier_name"]."',".$value["score6"].",'".$value["created_by"]."'),";
+			$csvData .="\n(".$c.",7,".$value["asn"].",'".$value["carrier_name"]."',".$value["score7"].",'".$value["created_by"]."'),";
+			$csvData .="\n(".$c.",8,".$value["asn"].",'".$value["carrier_name"]."',".$value["score8"].",'".$value["created_by"]."'),";
+			$csvData .="\n(".$c.",9,".$value["asn"].",'".$value["carrier_name"]."',".$value["score9"].",'".$value["created_by"]."'),";
+			$csvData .="\n(".$c.",10,".$value["asn"].",'".$value["carrier_name"]."',".$value["score10"].",'".$value["created_by"]."'),";			
 		}
 
 		return $csvData;
