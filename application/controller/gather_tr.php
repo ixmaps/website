fd8eeb4c402c3389c586977ca15fef154ba3a32c
Antonio Gamba-Bari <lodeanto@gmail.com>
2015-05-12 13:00:12 -0400

    degub tr-gather

--- application/controller/gather_tr.php
+++ application/controller/gather_tr.php
@@ -36,9 +36,7 @@ $_REQUEST['privacy'] = 8;
 $_REQUEST['submitter_ip'] = $myIp;
 $_REQUEST['status'] = "c";
 
-print_r($_REQUEST);
-
-echo "test";
+//print_r($_REQUEST);
 
 if(isset($_REQUEST['dest_ip']) && $_REQUEST['dest_ip']=="...")
 {
