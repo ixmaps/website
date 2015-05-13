0df5d1e961c130ded3f5decc25988dab725fa2bd
Antonio Gamba-Bari <lodeanto@gmail.com>
2015-05-13 19:20:03 -0400

    test TR collector

--- application/controller/gather_tr.php
+++ application/controller/gather_tr.php
@@ -4,10 +4,9 @@ include('../config.php');
 include('../model/GatherTr.php');
 
 // MaxMind Include Files
-/*include('../geoip/geoip.inc');
+include('../geoip/geoip.inc');
 include('../geoip/geoipcity.inc');
 include('../geoip/geoipregionvars.php');
-
 
 $dbQueryHtml = '';
 
@@ -18,41 +17,37 @@ $mtime = $mtime[1] + $mtime[0];
 $starttime = $mtime; 
 
 // using MaxMind to find the city/country of client's IP address
-$myIp = $_SERVER['REMOTE_ADDR'];
-$myCity = '';
-$gi1 = geoip_open("../geoip/dat/GeoLiteCityv6.dat",GEOIP_STANDARD);
+$myIp='201.245.56.136';
+//$myIp = $_SERVER['REMOTE_ADDR'];
+$gi1 = geoip_open("../geoip/dat/GeoLiteCity.dat",GEOIP_STANDARD);
 $record1 = geoip_record_by_addr_v6($gi1,"::".$myIp);
 $_REQUEST['city'] = ''.$record1->city;
 $_REQUEST['country'] = ''.$record1->country;
-geoip_close($gi1);*/
+geoip_close($gi1);
 
 //echo json_encode($_REQUEST);
 
-// add extra data 
-$_REQUEST['city'] = '..';
-$_REQUEST['country'] = '..';
+// FIXMW: add extra data 
 $_REQUEST['privacy'] = 8;
-//$_REQUEST['submitter_ip'] = "10.10.0.1";
-$_REQUEST['submitter_ip'] = '..';
-$_REQUEST['status'] = "c";
+$_REQUEST['submitter_ip'] = $myIp;
 
-//print_r($_REQUEST);
-
+print_r($_REQUEST);
+/*
 if(isset($_REQUEST['dest_ip']) && $_REQUEST['dest_ip']!="")
 {
 	//print_r($_REQUEST);	
 	$tr_c_id = GatherTr::saveContribution($_REQUEST);
-	echo "\ntr_c_id: ". $tr_c_id;
+	echo "\ntr_c_id: ". $tr_c_id."\n";
 	$b = GatherTr::saveContributionData($_REQUEST,$tr_c_id);
 	if($b==1){
 		$c = GatherTr::getContribution($tr_c_id);
-		//print_r($c);
-		$d = GatherTr::processTrData($tr_c_id); 
+		print_r($c);
+		//$d = GatherTr::processTrData($tr_c_id); 
 	} else {
 		// undo transaction...
 	}
 
 } else {
 	echo '<br/><hr/>No parameters sent.';
-}
+}*/
 ?>
