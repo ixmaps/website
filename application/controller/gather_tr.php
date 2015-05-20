5342b75f611ac15656d104ca81406acf31aeabb6
Antonio Gamba-Bari <lodeanto@gmail.com>
2015-05-20 19:02:24 -0400

    added TR analysis and publication in officil tables

--- application/controller/gather_tr.php
+++ application/controller/gather_tr.php
@@ -1,6 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *'); 
-ini_set( "display_errors", 0);
+//ini_set( "display_errors", 0); // use only in production 
 include('../config.php');
 include('../model/GatherTr.php');
 
@@ -9,55 +9,54 @@ include('../geoip/geoip.inc');
 include('../geoip/geoipcity.inc');
 include('../geoip/geoipregionvars.php');
 
-$dbQueryHtml = '';
-
-// vars for calculating excecution time 
-$mtime = microtime(); 
-$mtime = explode(" ",$mtime); 
-$mtime = $mtime[1] + $mtime[0]; 
-$starttime = $mtime; 
-
 // using MaxMind to find the city/country of client's IP address
 //$myIp='201.245.56.136';
-
 $myIp = $_SERVER['REMOTE_ADDR'];
-$_REQUEST['submitter_ip'] = $myIp;
+
+$_POST['submitter_ip'] = $myIp;
 $gi1 = geoip_open("../geoip/dat/GeoLiteCity.dat",GEOIP_STANDARD);
 $record1 = geoip_record_by_addr($gi1,$myIp);
-//$_REQUEST['city'] = ''.$record1->city;
-$_REQUEST['city'] = '';
-$_REQUEST['country'] = ''.$record1->country_code;
+$city = ''.$record1->city;
+$_POST['city'] = mb_convert_encoding($city, "UTF-8"); // Convert city name to UTF-8 encoding
+$_POST['country'] = ''.$record1->country_code;
 geoip_close($gi1);
 
-//echo json_encode($_REQUEST);
+//echo json_encode($_POST);
 
-// FIXME: add extra data 
-$_REQUEST['privacy'] = 8;
+// TODO: Analysis of Legacy code
+$_POST['privacy'] = 8;
+$_POST['client'] = "";
+$_POST['cl_version'] = "";
 
+//print_r($_POST);
 
-//print_r($_REQUEST);
-
-
-
-if(isset($_REQUEST['dest_ip']) && $_REQUEST['dest_ip']!="")
+/*
+	TODO: add  exahustive check for consistency and completness of the TR data 
+	Requires discussion with tests on the TrGen client
+*/
+if(isset($_POST['dest_ip']) && $_POST['dest_ip']!="")
 {
-	//print_r($_REQUEST);	
-	$tr_c_id = GatherTr::saveContribution($_REQUEST);
-	echo "\ntr_c_id: ". $tr_c_id."\n";
-	$b = GatherTr::saveContributionData($_REQUEST,$tr_c_id);
-	if($b==1){
-		$c = GatherTr::getContribution($tr_c_id);
+	//print_r($_POST);	
+	$tr_c_id = GatherTr::saveTrContribution($_POST);
+	//echo "\ntr_c_id: ". $tr_c_id."\n";
+	$b = GatherTr::saveTrContributionData($_POST,$tr_c_id);
+	
+	// use only for debugging 
+	/*
+		$c = GatherTr::getTrContribution($tr_c_id);
 		echo "TR Data saved!\n\n";
-
 		//print_r($c);
-		$d = GatherTr::processTrData($tr_c_id); 
-		echo "Analyzing TR data from ixnode: \n\n";
-		print_r($d);
-	} else {
-		// undo transaction...
-	}
+	*/
+	
+	$d = GatherTr::analyzeTrData($tr_c_id); 
+	//echo "Analyzing TR data: \n\n";
+	//print_r($d);
+	$trId = GatherTr::publishTraceroute($d);
+	//$f = GatherTr::flagContribution($tr_c_id, $trId); 
+	$result = array("TRid"=>$trId);
+	echo json_encode($result);
 
 } else {
-	echo '<br/><hr/>No parameters sent.';
+	echo 'No parameters sent.';
 }
 ?>
--- application/model/GatherTr.php
+++ application/model/GatherTr.php
@@ -1,26 +1,20 @@
 <?php
-
 class GatherTr
 {
 
 	/**
-		Saves all incomming traceroute data from TRgen V.2 into a temporary table (Parent)
+		Save incomming traceroute data (Header)
 	*/
-	public static function saveContribution($data) 
+	public static function saveTrContribution($data) 
 	{
 		global $dbconn, $ixmaps_debug_mode;
-
-		// FIXMEL: check if all mandatory fields are set
 
 		$sql = "INSERT INTO tr_contributions (traceroute_id, sub_time, dest, dest_ip, city, country, submitter, submitter_ip, submitter_os, postal_code, privacy, timeout, queries, maxhops, tr_flag) VALUES (NULL, NOW(), $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING tr_c_id;";
 
 		$trData = array($data['dest'], $data['dest_ip'], $data['city'], $data['country'], $data['submitter'], $data['submitter_ip'], $data['os'], $data['postal_code'], $data['privacy'], $data['timeout'], $data['queries'], $data['maxhops'], 0);
 
-		//echo $sql;
-
-		//print_r($trData);
-
-		$result = pg_query_params($dbconn, $sql, $trData) or die('saveContribution: Query failed: incorrect parameters'.pg_last_error());
+		$result = pg_query_params($dbconn, $sql, $trData) or die('saveTrContribution: Query failed: incorrect parameters'.pg_last_error());
+		//$result = pg_query($dbconn, $sql1) or die('saveContribution: Query failed: incorrect parameters'.pg_last_error());
 		$lastId = pg_fetch_all($result);
 		$tr_c_id = $lastId[0]['tr_c_id'];
 		pg_free_result($result);
@@ -28,15 +22,12 @@ class GatherTr
 	}
 
 	/**
-		Saves all incomming traceroute data from TRgen V.2 into a temporary table (child data)
+		Save incomming traceroute data (Contributions)
 	*/
-	public static function saveContributionData($data, $tr_c_id) 
+	public static function saveTrContributionData($data, $tr_c_id) 
 	{
 		global $dbconn, $ixmaps_debug_mode;
 
-		// FIXME!	check if all mandatory fields are set and return error
-		
-		// insert a new record for each $data['traceroute_submissions']
 		foreach ($data['traceroute_submissions'] as $key => $trDataItem) {
 
 			if(is_array($trDataItem['tr_data'])){				
@@ -50,11 +41,7 @@ class GatherTr
 			
 			$trData = array($trDataItem['client'], $trDataItem['protocol'], $trDataItem['data_type'],'...cmd', $trDataItem['tr_data'], 0);
 
-			//print_r($trData);
-
-			// FIXME: debug if SQL error
 			$result = pg_query_params($dbconn, $sql, $trData) or die('saveContributionData: Query failed: incorrect parameters'.pg_last_error());
-
 		}
 
 		pg_free_result($result);
@@ -63,10 +50,11 @@ class GatherTr
 
 	}
 
+	//TODO: optimize! evaluate SQL vs non-SQL storage 
 	/**
 		Get TR contribution
 	*/
-	public static function getContribution($tr_c_id) 
+	public static function getTrContribution($tr_c_id) 
 	{
 		global $dbconn, $ixmaps_debug_mode;
 		
@@ -74,15 +62,11 @@ class GatherTr
 		$sql2 = "SELECT tr_contribution_data.* FROM tr_contribution_data WHERE tr_c_id=$1;";
 		$sqlParams1 = array($tr_c_id);
 		$sqlParams2 = array($tr_c_id);
-		$result1 = pg_query_params($dbconn, $sql1, $sqlParams1) or die('getContribution: Query1 failed');
-		$result2 = pg_query_params($dbconn, $sql2, $sqlParams2) or die('getContribution: Query2 failed');
+		$result1 = pg_query_params($dbconn, $sql1, $sqlParams1) or die('getTrContribution: tr_contributions failed');
+		$result2 = pg_query_params($dbconn, $sql2, $sqlParams2) or die('getTrContribution: tr_contribution_data failed');
 		$dataArr1 = pg_fetch_all($result1);
 		$dataArr2 = pg_fetch_all($result2);
 		$dataArr1[0]['traceroute_submissions'] = $dataArr2;
-
-		//print_r($dataArr1[0]);
-		//print_r($dataArr2);
-		//print_r($contribArr);
 
 		pg_free_result($result1);
 		pg_free_result($result2);
@@ -91,45 +75,30 @@ class GatherTr
 	}
 
 	/**
-		Analyze TR data and return formated array for official submision
+		Analyze TR data
 	*/
-	public static function processTrData($tr_c_id) 
+	public static function analyzeTrData($tr_c_id) 
 	{
-		global $dbconn, $ixmaps_debug_mode;
-		$data = GatherTr::getContribution($tr_c_id);
+		global $dbconn;
+		$data = GatherTr::getTrContribution($tr_c_id);
 		//print_r($data);
 
 		$TR = array();
-
-		//$TR['header'] = $data;
-
+		// submissions
 		foreach ($data['traceroute_submissions'] as $key1 => $submission) {
 			
-			// parse tr_data: ixnode raw-sockets
-			if($submission['data_type']=='json'){
-				//echo "\n  json";
-				//echo "\n queries: ".$data['queries'];
+			// check contribution type
+			if($submission['data_type']=='json'){				
+				$trHops = json_decode($submission['tr_data'], true);
+				$totHops = count($trHops);
 				
-				$TrHops = json_decode($submission['tr_data'], true);
-				
-				//print_r($TrHops);
-
-				$totHops = count($TrHops);
-
-				//$TR['totHops'] = $totHops;
-
-				//echo "\n Total Hops: ".$totHops."\n";
-
-				foreach ($TrHops as $key2 => $hop) {
+				//hops
+				foreach ($trHops as $key2 => $hop) {
 					$hopNum = $key2+1;
-
-					//echo "\n Hop: ".$hopNum."\n";
-					//print_r($hop);
-
 					$latencies = array();
 					$ip_rank = array();
 
-					// loop passes/queries
+					// queries
 					foreach ($hop as $key3 => $hopPass) {
 						
 						//$hopPass['rtt'] = $hopPass['rtt']*100;
@@ -145,20 +114,15 @@ class GatherTr
 								$ip_rank[$hopPass['ip']] += 1;
 							}
 						} else {
-							// error at this pass/hop
+							// error at this hop
 						}
 
 						//
 
-					} // end pass/hop
-
+					} // end queries
 
 					sort($latencies);
-					//print_r($latencies);
-					arsort($ip_rank);
-					//print_r($ip_rank);
-					//reset($ip_rank);
-					
+					arsort($ip_rank);					
 					$keys=array_keys($ip_rank);
 					$winnerIp = $keys[0];
 
@@ -167,17 +131,13 @@ class GatherTr
 					$TR['hops'][$hopNum]['latencies'] = $latencies;
 					$TR['hops'][$hopNum]['winIp'] = $winnerIp;
 					
-					//$TR['hops'][$hopNum]['ip_rtt'] = $ip_latencies;
-					
-
 				} //end hop
-
-				//echo "\n";
-				print_r($TR);
-				//return $TR;
+				$data['ip_analysis'] = $TR;
+				return $data;
 
 			} else if($submission['data_type']=='txt'){
 				//echo "\nis txt";
+				//$data = GatherTr::analyzeRawTracerouteTxt($tr_c_id, );			
 
 			}
 			
@@ -185,17 +145,172 @@ class GatherTr
 
 	}
 
-	public static function saveTraceroute($data) 
+
+	/**
+	Publish TR data 
+	*/
+	public static function publishTraceroute($data) 
 	{
 		global $dbconn, $ixmaps_debug_mode;
 
-		$URI = "";
-		$TrString = "";
+		$URI = "http://www.ixmaps.ca/cgi-bin/gather-tr.cgi";
+		$trSubString = "";
 
-		foreach ($data as $key => $value) {
-			# code...
+		$trString ="";
+		
+		/*check tr status: does the TR reach its destination?*/
+		end($data['ip_analysis']['hops']);
+		$lastKey = key($data['ip_analysis']['hops']);
+		$lastIp = $data['ip_analysis']['hops'][$lastKey]['winIp'];
+		//echo "LastIP: ".$lastIp;
+
+		if($data['dest_ip']==$lastIp){
+			$trStatus = "c";
+		} else {
+			$trStatus = "i";
+		}
+
+		//echo "\nProtocol: ".$data['traceroute_submissions'][0]['protocol'];
+
+		if($data['traceroute_submissions'][0]['protocol']=="icmp"){
+			$protocol = "i";
+		} else if($data['traceroute_submissions'][0]['protocol']=="udp"){ 
+			$protocol = "u";
+		} else if($data['traceroute_submissions'][0]['protocol']=="tcp"){ 
+			$protocol = "t";
+		}		
+
+		$trString = "dest=".$data['dest']."&dest_ip=".$data['dest_ip']."&submitter=".urlencode($data['submitter'])."&zip_code=".urlencode($data['postal_code'])."&client=".urlencode($data['traceroute_submissions'][0]['client'])."&cl_ver=1.0&privacy=8&timeout=".$data['timeout']."&protocol=".$protocol."&maxhops=".$data['maxhops']."&attempts=".$data['queries']."&status=".$trStatus;
+		
+		$hopCount=0;
+
+		$foundFirstValidIp = false;
+		
+		// hops
+		foreach ($data['ip_analysis']['hops'] as $key => $hop) {
+
+			// skip local ips
+			if(!GatherTr::checkIpIsPrivate($hop['winIp']) || $hop['winIp']==""){
+
+				// anonimize first valid ip
+				if(!$foundFirstValidIp && $hop['winIp']!=""){
+					$foundFirstValidIp=true;
+					//echo "\n First Valid IP: ".$hop['winIp'];
+					$ipQuads = explode('.', $hop['winIp']);
+					$ipAmonim = "";
+
+					for ($i=0; $i < count($ipQuads); $i++) { 
+						if($i==count($ipQuads)-1){
+							$ipAmonim.=".0";
+
+						} else if ($i==0) {
+							$ipAmonim.= "".$ipQuads[$i];
+
+						} else {
+							$ipAmonim.= ".".$ipQuads[$i];
+						}
+					}
+					$hop['winIp'] = $ipAmonim;
+				}
+
+				$hopCount++;
+				$latencyCount = 0;
+				// latencies
+				foreach ($hop['latencies'] as $key1 => $latency) {
+					$latencyCount++;
+					
+					$rtt_ms=0;
+					
+					if($latency==-1)
+					{
+						$status="t";
+					} else {
+						$status="r";
+					}
+
+					$trString .= "&status_".$hopCount."_".$latencyCount."=".$status."&ip_addr_".$hopCount."_".$latencyCount."=".$hop['winIp']."&rtt_ms_".$hopCount."_".$latencyCount."=".round($latency);
+				}
+
+			} else {
+				//echo "\n skiping ip: ".$hop['winIp'];
+			} // end skip ip
+
+		}
+		$totItems = $hopCount*$data['queries'];
+		$trString .= "&n_items=".$totItems;
+		//echo "\n".$trString."";
+
+		// publish data
+		$trResult = file_get_contents($URI."?".$trString);
+		
+		//echo "\n\n".$trResult;
+
+		$search      = "new traceroute ID";
+		//$lines       = file('example.txt');
+		$line_number = false;
+		$tr_id_arr = explode("\n", $trResult);
+		while (list($key, $line) = each($tr_id_arr) and !$line_number) {
+		   $line_number = (strpos($line, $search) !== FALSE) ? $key + 1 : $line_number;
+		}
+		$tr_id_line = explode("=", $tr_id_arr[$line_number-1]);
+
+		//echo "\nTR ID: ".$tr_id_line[1];
+
+		if(count($tr_id_line)==2 && $tr_id_line[1]!=0){
+			return $tr_id_line[1];	
+		} else {
+			return 0;
 		}
 
 	}
+
+	/**
+		...
+	*/
+
+	public static function analyzeRawTracerouteTxt($data) 
+	{
+
+	}
+
+	/**
+		...
+	*/
+
+	public static function showTrContribution($data) 
+	{
+
+	}
+
+	/**
+	Determine if the IP is Private/Reserved
+	*/
+
+	public static function checkIpIsPrivate($ip) 
+	{
+		if (!filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	Flag TR contribution
+	*/
+
+	public static function flagContribution($tr_c_id, $traceroute_id)
+	{
+		global $dbconn;
+		if($tr_c_id!=0 && $traceroute_id!=0){
+			$sql = "UPDATE tr_contributions SET traceroute_id = $traceroute_id, tr_flag=1 WHERE tr_c_id = $tr_c_id";
+			$result = pg_query($dbconn, $sql) or die('flagContribution: Query failed'.pg_last_error());
+			pg_free_result($result);
+			return true;
+		} else {
+			return false;
+		}		
+	}
+		
 }
 ?>
