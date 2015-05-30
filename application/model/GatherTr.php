06e1151ca713061a98ec9b9a0c5a8c9728e2b2ec
agamba <lodeanto@gmail.com>
2015-05-30 13:11:42 -0400

    testing hostname change

--- application/model/GatherTr.php
+++ application/model/GatherTr.php
@@ -476,6 +476,7 @@ class GatherTr
 	public static function getHostnames($ip='';){
 		global $dbconn;
 		$sql = "SELECT ip_addr, hostname FROM ip_addr_info where ip_addr > '".$ip."' order by ip_addr DESC LIMIT 1000";
+		echo $sql;
 		$result = pg_query($dbconn, $sql) or die('getHostnames: Query failed'.pg_last_error());
 		$ip_host_data = pg_fetch_all($result);
 		pg_free_result($result);
