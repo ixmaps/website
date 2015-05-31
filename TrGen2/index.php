b3105cdb24272661c74d8c0a6ccd0356dd86f872
agamba <lodeanto@gmail.com>
2015-05-31 13:48:26 -0400

    added Welcome page for TRv2

---
+++ TrGen2/index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>IXmaps TrGen</title>
+  </head>
+  <body>
+    <div id="main">
+    <H3>Welcome to IX Traceroute Generator V.2</H3>
+    </div>
+  </body>
+</html>
