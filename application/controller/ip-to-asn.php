1316fa52de52edb03ed7b42e5dff0940c0e3cdaa
agamba <lodeanto@gmail.com>
2015-06-02 20:50:09 -0400

    adding ip-to-asn and hostname lookup

---
+++ application/controller/ip-to-asn.php
@@ -0,0 +1,23 @@
+<?php
+//header('Content-type: application/json');
+//header('Access-Control-Allow-Origin: *'); 
+include('../config.php');
+include('../model/GatherTr.php');
+
+$ip = $_REQUEST['ip'];
+
+/*
+  IP to AS using DB:
+ */
+
+  $asn_carrier = GatherTr::getIpForAsn($ip);
+  echo "<hr/>getIpForAsn...";
+  echo "<br/>netmask: ".$asn_carrier[0]['netmask'];
+  echo "<br/>asn: ".$asn_carrier[0]['asn'];
+  echo "<br/>Carrier: ".$asn_carrier[0]['name'];
+
+  echo "<hr/>getHostname...";
+  $hostname_new = GatherTr::getHostname($ip);
+  echo "<br/>netmask: ".$hostname_new;
+
+?>
