4f265751c61a8c29eb7cf45111110bcd4946ecd8
Colin McCann <colindmccann@gmail.com>
2015-07-30 12:27:27 -0400

    disabling analytics for explore

--- explore.php
+++ explore.php
@@ -95,18 +95,19 @@ geoip_close($gi1);
     document.write(script);
   </script>
 
+  <!-- ISSUES WITH SSL, DISABLING FOR NOW -->
   <script type="text/javascript">
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-24555700-1']);
-    _gaq.push(['_setDomainName', 'none']);
-    _gaq.push(['_setAllowLinker', true]);
-    _gaq.push(['_trackPageview']);
+    // var _gaq = _gaq || [];
+    // _gaq.push(['_setAccount', 'UA-24555700-1']);
+    // _gaq.push(['_setDomainName', 'none']);
+    // _gaq.push(['_setAllowLinker', true]);
+    // _gaq.push(['_trackPageview']);
 
-    (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
+    // (function() {
+    //   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    //   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    //   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    // })();
   </script>
 
   <script language="JavaScript" type="text/javascript">
