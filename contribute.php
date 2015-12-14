a5857b2a3a169a00f9b878fa8ecd4f4384bd5961
agamba <lodeanto@gmail.com>
2015-12-14 00:53:11 -0500

    Changed install instructions for Mac OS and Linix

--- contribute.php
+++ contribute.php
@@ -93,8 +93,12 @@
 				<li>Mac OS X v10.6 or higher (tested on 10.6.8, 10.7.5, 10.8,10.10. If you would like to help with updating the Mac version please <a href="mailto:ixmaps@utoronto.ca?subject=&#91;IXmaps%20Contribute]">get in touch</a>)<li>
 				<li>1GB of RAM</li>
 			</ul>
-			<h4>Mac OSX and Linux installation</h4>
-			<p><a href="https://www.ixmaps.ca/TrGen/IXmapsClient_v.1.0.dmg">Download the installer from here</a> and follow the instructions in the ReadMe file. You will need administrator privileges to run and install the software. You may also need to adjust your privacy and security settings to allow installing applications downloaded from the internet.</p>
+			<h4>Mac OSX installation</h4>
+			<p><a href="https://www.ixmaps.ca/TrGen/IXmapsClient_v.1.0.1.dmg">Download the installer from here - IXmapsClient_v.1.0.1.dmg</a> and follow the instructions in the ReadMe file. You will need administrator privileges to run and install the software. You may also need to adjust your privacy and security settings to allow installing applications downloaded from the internet.</p>
+
+			<h4>Linux installation</h4>
+			<p><a href="https://www.ixmaps.ca/TrGen/IXmapsClient_v.1.0.dmg">Download the installer from here - IXmapsClient_linux_v.1.0.zip</a> and follow the instructions in the ReadMe file. You will need administrator privileges to run and install the software. </p>
+
 			<h4>Windows installation</h4>
 			<p>To install (an older) version of our traceroute generator (TrGen), <a href="https://www.ixmaps.ca/TrGen/trgen-0.8.8.msi">download the Windows installer from here</a> and follow the instructions. You will not need administrator privileges to run the software once installed, but you may need to be logged into an administrator account to install it. (We welcome help from those with Windows installation experience in creating a Windows package for the latest version of IXmaps Client software.)</p>
 
