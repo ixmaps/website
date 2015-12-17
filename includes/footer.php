ebed5ec8f28307ba2a7c1a0aee14c90f58741988
Colin McCann <colindmccann@gmail.com>
2015-12-17 17:35:00 -0500

    finalized twitter button stuff. yuck

--- includes/footer.php
+++ includes/footer.php
@@ -79,43 +79,46 @@ include('application/config.php');
     </div>
 
     <aside class="footer-segment">
-        <p>The IXmaps website is licensed under a <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.en_US" target="_blank" alt=" CC Attribution-NonCommercial-ShareAlike 4.0 International"> Creative Commons Attribution-ShareAlike 4.0 International License</a>.
 
-    <?php
-      //  fixed by ANTO
-      $doRecursive = '';
-      $getLastModDir = mostRecentModifiedFileTime($appRootPath."/.",$doRecursive);
+      <p>The IXmaps website is licensed under a <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.en_US" target="_blank" alt=" CC Attribution-NonCommercial-ShareAlike 4.0 International"> Creative Commons Attribution-ShareAlike 4.0 International License</a>.
 
-      date_default_timezone_set('America/Toronto');
-      echo "<br/>Last modified " . date("l, dS F, Y @ h:ia", $getLastModDir) . '<br/>';
+      <?php
+        //  fixed by ANTO
+        $doRecursive = '';
+        $getLastModDir = mostRecentModifiedFileTime($appRootPath."/.",$doRecursive);
 
-      function mostRecentModifiedFileTime($dirName,$doRecursive) {
-        $d = dir($dirName);
-        $lastModified = 0;
-        $currentModified = 0;
-        while($entry = $d->read()) {
-          if ($entry != "." && $entry != "..") {
-            if (!is_dir($dirName."/".$entry)) {
-              $currentModified = filemtime($dirName."/".$entry);
-            } else if ($doRecursive && is_dir($dirName."/".$entry)) {
-              $currentModified = mostRecentModifiedFileTime($dirName."/".$entry,true);
-            }
-            if ($currentModified > $lastModified){
-              $lastModified = $currentModified;
+        date_default_timezone_set('America/Toronto');
+        echo "Last modified " . date("l, dS F, Y @ h:ia", $getLastModDir) . '<br/>';
+
+        function mostRecentModifiedFileTime($dirName,$doRecursive) {
+          $d = dir($dirName);
+          $lastModified = 0;
+          $currentModified = 0;
+          while($entry = $d->read()) {
+            if ($entry != "." && $entry != "..") {
+              if (!is_dir($dirName."/".$entry)) {
+                $currentModified = filemtime($dirName."/".$entry);
+              } else if ($doRecursive && is_dir($dirName."/".$entry)) {
+                $currentModified = mostRecentModifiedFileTime($dirName."/".$entry,true);
+              }
+              if ($currentModified > $lastModified){
+                $lastModified = $currentModified;
+              }
             }
           }
+          $d->close();
+          return $lastModified;
         }
-        $d->close();
-        return $lastModified;
-      }
-    ?>
+      ?>
 
-    <p>Please <a href="mailto: ixmaps@utoronto.ca?subject=IXmaps Website" class="smallinks">contact the site admin</a>
-      with any questions or concerns.</p>
-    <p>To view our privacy policy, please <a href="/privacy.php" target="_blank">click here</a>.</p>
+      <!-- <p>Please <a href="mailto: ixmaps@utoronto.ca?subject=IXmaps Website" class="smallinks">contact the site admin</a> with any questions or concerns.</p> -->
+      <p>To view our privacy policy, please <a href="/privacy.php" target="_blank">click here</a>.</p>
 
-    <a href="https://twitter.com/share" class="twitter-share-button"{count} data-url="http://ixmaps.ca" data-text="IXmaps">Tweet</a>
-    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+      <div id="footer-button-container">
+        <a href="https://twitter.com/share" class="twitter-share-button"{count} data-text="See where your internet communication is intercepted by the NSA. Check out @IXmapsCA's internet mapping tool at">Tweet</a>
+        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+        <a id="feedback-button" href="mailto: ixmaps@utoronto.ca?subject=IXmaps Website">Feedback</a>
+      </div>
     </aside><!-- end of #third footer segment -->
   </section><!-- end of footer-outer-block -->
 </section><!-- end of footer-area -->
