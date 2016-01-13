f2984c569d8e71b535a53d81f54f157a6573d03b
agamba <lodeanto@gmail.com>
2016-01-13 12:02:01 -0500

    geoip now using MM data downloaded by cron job

--- application/geoip/index.php
+++ application/geoip/index.php
@@ -1,7 +1,5 @@
-
 <?php
-//#!/usr/bin/php -q
-
+/* Last update: by ANTO - Jan 13, 2016*/
 // This code demonstrates how to lookup the country, region, city,
 // postal code, latitude, and longitude by IP Address.
 // It is designed to work with GeoIP/GeoLite City
@@ -9,6 +7,8 @@
 // Note that you must download the New Format of GeoIP City (GEO-133).
 // The old format (GEO-132) will not work.
 
+// Data stored in ~/ix-data/mm-data
+include('../config.php');
 include("geoip.inc");
 include("geoipcity.inc");
 include("geoipregionvars.php");
@@ -17,30 +17,17 @@ if(isset($_POST['ip']))
 {
 	$ip=$_POST['ip'];
 
-/*	print $recordIx->country_code . " " . $record->country_code3 . " " . $record->country_name . "<br/>";
-	print $recordIx->region . " " . $GEOIP_REGION_NAME[$record->country_code][$record->region] . "<br/>";
-	print $recordIx->city . "<br/>";
-	print $recordIx->postal_code . "<br/>";
-	print $recordIx->latitude . "<br/>";
-	print $recordIx->longitude . "<br/>";
-	print $recordIx->metro_code . "<br/>";
-	print $recordIx->area_code . "<br/>";
-	print $recordIx->continent_code . "<br/>";
-*/	
-
 } else {
 	//$ip = '';
 	$ip=$_SERVER['REMOTE_ADDR'];
 }
 
-$giIx = geoip_open("dat/GeoLiteCity.dat",GEOIP_STANDARD);
+$giIx = geoip_open($MM_dat_dir."/GeoLiteCity.dat",GEOIP_STANDARD);
 $recordIx = geoip_record_by_addr($giIx,$ip);
 $lat = $recordIx->latitude;
 $long = $recordIx->longitude;
 geoip_close($giIx);
-
 ?>
-
 
 <!DOCTYPE html>
 <html>
@@ -75,7 +62,6 @@ geoip_close($giIx);
           mapTypeId: google.maps.MapTypeId.ROADMAP
         };
         var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
-////////
 		var ip_IXmaps = new google.maps.LatLng(<?php echo $lat.','.$long;?>)
 		var marker_IXmaps = new google.maps.Marker({
 	      position: ip_IXmaps,
@@ -83,7 +69,7 @@ geoip_close($giIx);
 	      title:'<?php echo $ip;?>'
 			});
 		//marker_IXmaps.setIcon('http://localhost/mywebapps/ixmaps.ca/dev.ixmaps.ca/images/hop1.png');
-///////
+
 	<?php } ?>
 
       }
@@ -91,7 +77,7 @@ geoip_close($giIx);
   </head>
   <body onload="initialize()">
   	<div id="main" style="float: left;">
-		<form action="index.php" method="post">
+		<form action="index.php" method="get">
 		IP: <input name="ip" type"text" value="<?php echo $ip;?>"/> <input type="submit" value="Geocode"/>
 		</form>
 <?php
@@ -99,29 +85,22 @@ if($ip!='')
 {
 	echo '<hr/>'.$ip.' ('.$lat.' , '.$long.')';
 	echo '<hr/><b>Using GeoIP.dat</b><br/>';
-	$gi0 = geoip_open("dat/GeoIP.dat",GEOIP_STANDARD);
+	$gi0 = geoip_open($MM_dat_dir."/GeoIP.dat",GEOIP_STANDARD);
 
 	echo geoip_country_code_by_addr($gi0, $ip) . "\t" .
 	     geoip_country_name_by_addr($gi0, $ip) . "<br/>";
 
 	geoip_close($gi0);
 
-	// uncomment for Shared Memory support
-	// geoip_load_shared_mem("/usr/local/share/GeoIP/GeoIPCity.dat");
-	// $gi = geoip_open("/usr/local/share/GeoIP/GeoIPCity.dat",GEOIP_SHARED_MEMORY);
-
-	//$gi = geoip_open("/usr/local/share/GeoIP/GeoLiteCityv6.dat",GEOIP_STANDARD);
-	
-
 	// ASN
 	echo '<hr/><b>Using GeoIPASNum.dat</b><br/>';
-	$giasn = geoip_open("dat/GeoIPASNum.dat", GEOIP_STANDARD);
+	$giasn = geoip_open($MM_dat_dir."/GeoIPASNum.dat", GEOIP_STANDARD);
 	$asn = geoip_name_by_addr($giasn, $ip);
 	echo "<br/>ASN: ".$asn;
 	geoip_close($giasn);	
 
 	echo '<hr/><b>Using GeoLiteCityv6.dat</b><br/>';
-	$gi = geoip_open("dat/GeoLiteCityv6.dat",GEOIP_STANDARD);
+	$gi = geoip_open($MM_dat_dir."/GeoLiteCityv6.dat",GEOIP_STANDARD);
 	//$record = geoip_record_by_addr_v6($gi,"::24.24.24.24");
 	$record = geoip_record_by_addr_v6($gi,"::".$ip);
 	print $record->country_code . " " . $record->country_code3 . " " . $record->country_name . "<br/>";
@@ -136,16 +115,8 @@ if($ip!='')
 	geoip_close($gi);
 
 
-
-/*	include("geoipcity.inc");
-	include("geoipregionvars.php");
-*/
-	// uncomment for Shared Memory support
-	// geoip_load_shared_mem("/usr/local/share/GeoIP/GeoIPCity.dat");
-	// $gi = geoip_open("/usr/local/share/GeoIP/GeoIPCity.dat",GEOIP_SHARED_MEMORY);
-
 	echo '<hr/><b>Using GeoLiteCity.dat</b><br/>';
-	$gi1 = geoip_open("dat/GeoLiteCity.dat",GEOIP_STANDARD);
+	$gi1 = geoip_open($MM_dat_dir."/GeoLiteCity.dat",GEOIP_STANDARD);
 	$record1 = geoip_record_by_addr($gi1,$ip);
 	print $record1->country_code . " " . $record->country_code3 . " " . $record->country_name . "<br/>";
 	print $record1->region . " " . $GEOIP_REGION_NAME[$record->country_code][$record->region] . "<br/>";
