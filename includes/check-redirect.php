ee859e68398117c18538709a2bddb8c8946a83de
agamba <lodeanto@gmail.com>
2016-07-08 13:54:01 -0400

    uptaded check-redirect

--- includes/check-redirect.php
+++ includes/check-redirect.php
@@ -8,16 +8,15 @@ $URI = $_SERVER["REQUEST_URI"];
 
 if ($ixHost != 'www.ixmaps.ca' && $ixHost != 'dev.ixmaps.ca') {
 	//header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
- 	header("Location: https://www.ixmaps.ca" . $URI;
+ 	header("Location: https://www.ixmaps.ca" . $URI);
  	exit();
 }
-
 
 
 // forcing https and checking whether server name is diferent from www.ixmaps.ca
 if (!isset($_SERVER['HTTPS'])){
   //header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
-	header("Location: https://www.ixmaps.ca" . $URI;
+	header("Location: https://www.ixmaps.ca" . $URI);
  	exit();
 }
 ?>
