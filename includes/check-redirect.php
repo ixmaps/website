d3411154b40f37a67386e2b7dd618c2fb61f04b0
agamba <lodeanto@gmail.com>
2016-07-08 14:07:49 -0400

    uptaded check-redirect

--- includes/check-redirect.php
+++ includes/check-redirect.php
@@ -6,15 +6,9 @@ $URI = $_SERVER["REQUEST_URI"];
 
 /*Force redirection  either http or https*/
 
-/*if ($ixHost != 'www.ixmaps.ca' && $ixHost != 'dev.ixmaps.ca') {
-	//header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
- 	header("Location: https://www.ixmaps.ca" . $URI);
- 	exit();
-}*/
-
-
 // forcing https and checking whether server name is diferent from www.ixmaps.ca
-if (!isset($_SERVER['HTTPS'])){
+if ((!isset($_SERVER['HTTPS'])) || ($ixHost != 'www.ixmaps.ca')){
+//if (!isset($_SERVER['HTTPS'])){
   //header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
 	header("Location: https://www.ixmaps.ca" . $URI);
  	exit();
