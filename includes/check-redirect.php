634189dab364482814a0df3126c7d8db1a2c82a1
agamba <lodeanto@gmail.com>
2016-07-08 13:52:34 -0400

    uptaded check-redirect

--- includes/check-redirect.php
+++ includes/check-redirect.php
@@ -1,23 +1,23 @@
 <?php
 // Preventing access from: ixmaps.ischool.utoronto.ca or ixmaps.ca
 $ixHost = $_SERVER["SERVER_NAME"];
-/*if ($ixHost != 'www.ixmaps.ca' && $ixHost != 'dev.ixmaps.ca') {
-  header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
-  exit();
-}*/
+//$ixHost = $_SERVER["HTTP_HOST"];
+$URI = $_SERVER["REQUEST_URI"];
 
 /*Force redirection  either http or https*/
 
-// forcing http
-/*if (isset($_SERVER['HTTPS'])){
-  header("Location: http://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
-  exit();
-}*/
+if ($ixHost != 'www.ixmaps.ca' && $ixHost != 'dev.ixmaps.ca') {
+	//header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+ 	header("Location: https://www.ixmaps.ca" . $URI;
+ 	exit();
+}
 
-// forcing https
+
+
+// forcing https and checking whether server name is diferent from www.ixmaps.ca
 if (!isset($_SERVER['HTTPS'])){
   //header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
-	header("Location: https://www.ixmaps.ca" . $_SERVER["REQUEST_URI"]);
+	header("Location: https://www.ixmaps.ca" . $URI;
  	exit();
 }
 ?>
