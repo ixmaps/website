3dc4efbddb034e8466f16304204d00b301cd3867
agamba <lodeanto@gmail.com>
2016-07-08 10:48:59 -0400

    uptaded check-redirect

--- includes/check-redirect.php
+++ includes/check-redirect.php
@@ -1,10 +1,10 @@
 <?php
-// Old redirection, preventing access from a ixmaps.ischool.utoronto.ca
-/*$ixHost = $_SERVER["SERVER_NAME"];
+// Preventing access from: ixmaps.ischool.utoronto.ca or ixmaps.ca
+$ixHost = $_SERVER["SERVER_NAME"];
 if ($ixHost != 'www.ixmaps.ca' && $ixHost != 'dev.ixmaps.ca') {
-  header("Location: http://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+  header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
   exit();
-}*/
+}
 
 /*Force redirection  either http or https*/
 
