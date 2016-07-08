fcc8290202f94f2225ca82a1819e37258932de19
agamba <lodeanto@gmail.com>
2016-07-07 22:45:44 -0400

    Removing GA and setting analytics include

--- transparency.php
+++ transparency.php
@@ -19,25 +19,8 @@
 
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 
-  <!-- Piwik -->
-  <script type="text/javascript">
-    var _paq = _paq || [];
-    _paq.push(['trackPageView']);
-    _paq.push(['enableLinkTracking']);
-    (function() {
-    var u="//ixmaps.piwikpro.com//";
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', 1]);
-    var d=document, g=d.createElement('script'),
-    s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true;
-    g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-    })();
-  </script>
-  <noscript>
-    <p><img src="//ixmaps.piwikpro.com/piwik.php?idsite=1" style="border:0;" alt="" /></p>
-  </noscript>
-  <!-- End Piwik Code -->
+  <!-- include analytics -->
+  <?php include("includes/analytics.php"); ?>
 </head>
 
 <body>
