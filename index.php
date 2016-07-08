58a72b9636432f8b71e413996c4f094828f43580
agamba <lodeanto@gmail.com>
2016-07-07 22:25:40 -0400

    Removing GA and setting analytics include

--- index.php
+++ index.php
@@ -23,39 +23,6 @@ $ixHost = $_SERVER["SERVER_NAME"];
 		var TODAY = monthname[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear();
 		//--------------- END LOCALIZEABLE ---------------
   </script>
-	<script type="text/javascript">
-		var _gaq = _gaq || [];
-		_gaq.push(['_setAccount', 'UA-24555700-1']);
-		_gaq.push(['_setDomainName', 'none']);
-		_gaq.push(['_setAllowLinker', true]);
-		_gaq.push(['_trackPageview']);
-
-		(function() {
-		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		})();
-	</script>
-
-	<!-- Piwik -->
-	<script type="text/javascript">
-    var _paq = _paq || [];
-    _paq.push(['trackPageView']);
-    _paq.push(['enableLinkTracking']);
-    (function() {
-    var u="//ixmaps.piwikpro.com//";
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', 1]);
-    var d=document, g=d.createElement('script'),
-    s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true;
-    g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-    })();
-	</script>
-	<noscript>
-		<p><img src="//ixmaps.piwikpro.com/piwik.php?idsite=1" style="border:0;" alt="" /></p>
-	</noscript>
-	<!-- End Piwik Code -->
 
 	<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
 	<script type="text/javascript">
@@ -79,11 +46,13 @@ $ixHost = $_SERVER["SERVER_NAME"];
 	</script>
 	<script src="//www.youtube.com/iframe_api"></script>
 
-
 	<!-- STYLESHEETS -->
 	<link rel="stylesheet" href="css/ix.css" type="text/css" />
 	<link rel="stylesheet" href="css/lightbox.css" type="text/css" media="screen" />
 	<link rel="stylesheet" href="css/overwrites.css" type="text/css" />
+
+	<!-- include analytics -->
+	<?php include("includes/analytics.php"); ?>
 </head>
 
 <body onload="initialize()">
