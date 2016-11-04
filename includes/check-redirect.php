ed96d1f5dfaca4d94803e10d6130c29eb24c1efd
agamba <lodeanto@gmail.com>
2016-11-04 15:34:48 -0400

    finalized dev setup - index.js and check-redirect.php and config.php need to be modified to for any switches

--- includes/check-redirect.php
+++ includes/check-redirect.php
@@ -1,16 +1,16 @@
 <?php
 // Preventing access from: ixmaps.ischool.utoronto.ca or ixmaps.ca
-$ixHost = $_SERVER["SERVER_NAME"];
+//$ixHost = $_SERVER["SERVER_NAME"];
 //$ixHost = $_SERVER["HTTP_HOST"];
-$URI = $_SERVER["REQUEST_URI"];
+//$URI = $_SERVER["REQUEST_URI"];
 
 /*Force redirection  either http or https*/
 
 // forcing https and checking whether server name is diferent from www.ixmaps.ca
-if ((!isset($_SERVER['HTTPS'])) || ($ixHost != 'www.ixmaps.ca')){
+//if ((!isset($_SERVER['HTTPS'])) || ($ixHost != 'www.ixmaps.ca')){
 //if (!isset($_SERVER['HTTPS'])){
   //header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
-	header("Location: https://www.ixmaps.ca" . $URI);
- 	exit();
-}
+//	header("Location: https://dev.ixmaps.ca" . $URI);
+// 	exit();
+//}
 ?>
