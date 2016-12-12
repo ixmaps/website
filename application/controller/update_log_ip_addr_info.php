00fe693416311393340cbbcd583d6e429f17b66f
agamba <lodeanto@gmail.com>
2016-12-11 19:26:42 -0500

    Added update_log_ip_addr_info

---
+++ application/controller/update_log_ip_addr_info.php
@@ -0,0 +1,11 @@
+<?php
+header('Access-Control-Allow-Origin: *'); 
+include('../config.php');
+//include('../model/IXmapsMaxMind.php'); 
+include('../model/IXmapsGeoCorrection.php'); 
+
+$limit = 100;
+$ipAddrData = IXmapsGeoCorrection::getIpAddrInfo($limit);
+$lastIp = IXmapsGeoCorrection::insertLogIpAddrInfo($ipAddrData);
+//echo "\n Last Ip: ".$lastIp;
+?>
