b228db00119b8f94291402caaee9a2f1762a12cb
agamba <lodeanto@gmail.com>
2016-12-15 22:15:54 -0500

    Testing Update geodata

--- application/model/IXmapsGeoCorrection.php
+++ application/model/IXmapsGeoCorrection.php
@@ -21,7 +21,7 @@ class IXmapsGeoCorrection
 			}
 
 		} else if($type==1){
-			$sql1 = "SELECT lat, long FROM ip_addr_info WHERE p_status='G';";
+			$sql1 = "SELECT ip_addr, lat, long FROM ip_addr_info WHERE p_status='G';";
 		}
 
 		$result1 = pg_query($dbconn, $sql1);
