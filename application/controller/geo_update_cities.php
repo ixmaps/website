f859847b0afbd94e6c8206c69a7bca7fef155a2f
agamba <lodeanto@gmail.com>
2017-02-09 04:34:13 -0500

    Added msg for results

--- application/controller/geo_update_cities.php
+++ application/controller/geo_update_cities.php
@@ -27,89 +27,94 @@ if(isset($_GET['m'])){
 /* collect parameters if run from the command line */
 //$params = getopt("f:hp:");
 
-
-// Update geodata
-foreach ($ipAddrData as $key => $ipData) {
-	$ipToGeoData = array();
-	//$ipToGeoData = IXmapsGeoCorrection::updateGeoData($ipData); // old
-	$ipToGeoData = IXmapsGeoCorrection::getClosestGeoData($ipData, $matchLimit);
-	
-	//print_r($ipToGeoData);
-
-	$bestMatchCountry = array ();
-	$bestMatchRegion = array ();
-	$bestMatchCity = array ();
-
-	// Add distance to each match
-	foreach ($ipToGeoData as $key1 => $geoLocMatch) {
-		$latitudeFrom = $ipAddrData[0]['lat'];
-		$longitudeFrom = $ipAddrData[0]['long'];
-		$latitudeTo = $geoLocMatch['latitude'];
-		$longitudeTo = $geoLocMatch['longitude'];
-		$distance = IXmapsGeoCorrection::distanceBetweenCoords($latitudeFrom, $longitudeFrom, $latitudeTo, $longitudeTo);
-		$ipToGeoData[$key1]['distance'] = $distance;
-
-		//var_dump($geoLocMatch);
+// check if nothing to do
+if(!$ipAddrData){
+	echo "\n"."Nothing to do.\n";
+} else {
+	// Update geodata
+	foreach ($ipAddrData as $key => $ipData) {
+		$ipToGeoData = array();
+		//$ipToGeoData = IXmapsGeoCorrection::updateGeoData($ipData); // old
+		$ipToGeoData = IXmapsGeoCorrection::getClosestGeoData($ipData, $matchLimit);
 		
-		// TODO add exclusion rule based on max distance
+		//print_r($ipToGeoData);
 
-		// Exclude null Country names
-		if($geoLocMatch['region']!=""){
-			if(!isset($bestMatchCountry[$geoLocMatch['region']])){
-				$bestMatchRegion[$geoLocMatch['region']] = 1;
-			} else {
-				$bestMatchRegion[$geoLocMatch['region']] += 1;	
+		$bestMatchCountry = array ();
+		$bestMatchRegion = array ();
+		$bestMatchCity = array ();
+
+		// Add distance to each match
+		foreach ($ipToGeoData as $key1 => $geoLocMatch) {
+			$latitudeFrom = $ipAddrData[0]['lat'];
+			$longitudeFrom = $ipAddrData[0]['long'];
+			$latitudeTo = $geoLocMatch['latitude'];
+			$longitudeTo = $geoLocMatch['longitude'];
+			$distance = IXmapsGeoCorrection::distanceBetweenCoords($latitudeFrom, $longitudeFrom, $latitudeTo, $longitudeTo);
+			$ipToGeoData[$key1]['distance'] = $distance;
+
+			//var_dump($geoLocMatch);
+			
+			// TODO add exclusion rule based on max distance
+
+			// Exclude null Country names
+			if($geoLocMatch['region']!=""){
+				if(!isset($bestMatchCountry[$geoLocMatch['region']])){
+					$bestMatchRegion[$geoLocMatch['region']] = 1;
+				} else {
+					$bestMatchRegion[$geoLocMatch['region']] += 1;	
+				}
 			}
-		}
 
-		// Exclude null Region names
-		if($geoLocMatch['country']!=""){
-			if(!isset($bestMatchCountry[$geoLocMatch['country']])){
-				$bestMatchCountry[$geoLocMatch['country']] = 1;
-			} else {
-				$bestMatchCountry[$geoLocMatch['country']] += 1;	
+			// Exclude null Region names
+			if($geoLocMatch['country']!=""){
+				if(!isset($bestMatchCountry[$geoLocMatch['country']])){
+					$bestMatchCountry[$geoLocMatch['country']] = 1;
+				} else {
+					$bestMatchCountry[$geoLocMatch['country']] += 1;	
+				}
 			}
-		}
 
-		// Exclude null city names
-		if($geoLocMatch['city']!=""){
-			if(!isset($bestMatchCity[$geoLocMatch['city']])){
-				$bestMatchCity[$geoLocMatch['city']] = 1;
-			} else {
-				$bestMatchCity[$geoLocMatch['city']] += 1;	
+			// Exclude null city names
+			if($geoLocMatch['city']!=""){
+				if(!isset($bestMatchCity[$geoLocMatch['city']])){
+					$bestMatchCity[$geoLocMatch['city']] = 1;
+				} else {
+					$bestMatchCity[$geoLocMatch['city']] += 1;	
+				}
 			}
-		}
 
-	} // end for find best match
-	
-    // add best match geoData
-	arsort($bestMatchCountry);
-	arsort($bestMatchRegion);
-	arsort($bestMatchCity);
+		} // end for find best match
+		
+	    // add best match geoData
+		arsort($bestMatchCountry);
+		arsort($bestMatchRegion);
+		arsort($bestMatchCity);
 
-	//print_r($bestMatchCity);
+		//print_r($bestMatchCity);
 
-	// add best match 
-    $ipAddrData[$key]["mm_country_update"] = key($bestMatchCountry);
-    $ipAddrData[$key]["mm_region_update"] = key($bestMatchRegion);
-    $ipAddrData[$key]["mm_city_update"] = key($bestMatchCity);
+		// add best match 
+	    $ipAddrData[$key]["mm_country_update"] = key($bestMatchCountry);
+	    $ipAddrData[$key]["mm_region_update"] = key($bestMatchRegion);
+	    $ipAddrData[$key]["mm_city_update"] = key($bestMatchCity);
 
-    // add all matches: for reference
-    $ipAddrData[$key]['closest_matches'] = $ipToGeoData;
+	    // add all matches: for reference
+	    $ipAddrData[$key]['closest_matches'] = $ipToGeoData;
 
-	//echo "\n"."Nearest GeoData (Country/City) found for (IP) Set: "."\n";
+		//echo "\n"."Nearest GeoData (Country/City) found for (IP) Set: "."\n";
 
-	//print_r($ipToGeoData);
-	//print_r($ipAddrData[$key]);
-	//echo json_encode($ipAddrData[$key]);
+		//print_r($ipToGeoData);
+		//print_r($ipAddrData[$key]);
+		//echo json_encode($ipAddrData[$key]);
 
-	// update 
-	$updateGeoData = IXmapsGeoCorrection::updateGeoData($ipAddrData[$key]);
+		// update 
+		$updateGeoData = IXmapsGeoCorrection::updateGeoData($ipAddrData[$key]);
 
-} // end for set of ips
+	} // end for set of ips
+	echo "\n"."done\n";
+} 
 
 //echo json_encode($ipAddrData);
-echo "\n"."done\n";
+
 //print_r($ipAddrData);
 
 
