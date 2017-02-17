29ec785ed1fc568609184bea3fff16edd3bb610f
agamba <lodeanto@gmail.com>
2017-02-17 15:53:55 -0500

    getting user geodata

--- application/controller/ixmapsMaxMind.php
+++ application/controller/ixmapsMaxMind.php
@@ -2,6 +2,7 @@
 header('Access-Control-Allow-Origin: *'); 
 include('../config.php');
 include('../model/IXmapsMaxMind.php'); 
+include('../model/IXmapsGeoCorrection.php'); 
 
 $ip=$_GET['ip'];
 $mm = new IXmapsMaxMind();
@@ -9,9 +10,14 @@ $geoIp = $mm->getGeoIp($ip);
 //print_r($geoIp);
 $mm->closeDatFiles();
 
-
 if($geoIp['geoip']['city']==null){
-	echo "city is null";
-}
+	$ipData = array(
+		"lat"=>$geoIp['geoip']['latitude'],
+		"long"=>$geoIp['geoip']['longitude']
+		);
+	$matchLimit = 10;
+	$ipToGeoData = IXmapsGeoCorrection::getClosestGeoData($ipData, $matchLimit);
+	$geoIp['matches']=$ipToGeoData;
+
 echo json_encode($geoIp);
 ?>
