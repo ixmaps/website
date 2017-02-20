d8f8036e9fa37478a21e9d45897bb96e189aa53d
agamba <lodeanto@gmail.com>
2017-02-19 20:23:17 -0500

    Added fixes to user loc

--- js/ixmaps.js
+++ js/ixmaps.js
@@ -633,30 +633,37 @@ var submitUserLocObject = function() {
   if (myCity!="" && myCountry!="" && myASN) {
     console.log('Searching based on ASN, Country, and City');
     userLocJSON = {
-      "parameters":
+    "parameters":
+    {
+      "submitOnLoad": true,
+      "submissionType": "customFilter",
+      "otherFunction": ""
+    },
+    "constraints":
+    {
+      "filter-constraint-1":
       {
-        "submitOnLoad": true,
-        "submissionType": "customFilter",
-        "otherFunction": ""
+        constraint1: "does",
+        constraint2: "originate",
+        constraint3: "asnum",
+        constraint4: myASN,
+        constraint5: "AND"
       },
-      "constraints":
+      "filter-constraint-2":
       {
-        "filter-constraint-1":
-        {
-          constraint1: "does",
-          constraint2: "originate",
-          constraint3: "asnum",
-          constraint4: myASN,
-          constraint5: "AND"
-        },
-        "filter-constraint-2":
-        {
-          constraint1: "does",
-          constraint2: "originate",
-          constraint3: "country",
-          constraint4: myCountry,
-          constraint5: "AND"
-        }
+        constraint1: "does",
+        constraint2: "originate",
+        constraint3: "city",
+        constraint4: myCity,
+        constraint5: "AND"
+      },
+      "filter-constraint-3":
+      {
+        constraint1: "does",
+        constraint2: "originate",
+        constraint3: "country",
+        constraint4: myCountry,
+        constraint5: "AND"
       }
     };
     var jsonToString = JSON.stringify(userLocJSON);
